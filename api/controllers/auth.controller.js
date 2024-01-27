41aab6900310a5374867901655f9b2f6aed46deb
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-01-27 18:23:50 +0200

    Create Login API Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const Signup = async(req,res,next)=>{
     const {username, email, password} = req.body;
@@ -25,4 +26,34 @@ export const Signup = async(req,res,next)=>{
     } catch (error) {
         next(error)
     }
+}
+
+export const Login = async(req,res,next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(400, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid Password'));
+        }
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        
+        const {password: pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie('Access_Token', token, {httpOnly: true}).json('Signin Successfully')
+
+    } catch (error) {
+        next(error)
+    }
+
+
 }
