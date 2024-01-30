3255c5c51935beb3860687fe4ecbc6cead75cbb3
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-01-30 18:30:31 +0200

    Update User API Route

--- api/index.js
+++ api/index.js
@@ -3,24 +3,25 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
-
+import cookieParser from 'cookie-parser';
+// Config Server
 const port = 3000
 dotenv.config()
 
-
 // Run Server
 const app = express();
+app.use(express.json())
+app.use(cookieParser())
 app.listen(port, ()=>{
     console.log('Server is running on Port '+ port);
-})
+});
 // Connect to MongoDB Database
 mongoose.connect(process.env.MONGO_CONFIG).then(()=>{
-    console.log("Database is Connected Successfully");
+    console.log("MONGODB is Connected");
 });
 // API Routes
-app.use(express.json())
-app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
 
 // Create Messages Errors
 app.use((err,req,res,next)=>{
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import {errorHandler} from '../utils/error.js'
+
+export const verifyToken = (req,res,next)=>{
+    const token = req.cookies.Access_Token;
+    console.log(token);
+    if(!token){
+        return next(errorHandler(401, 'Unauthorized'))
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err,user)=>{
+        if(err){
+            return next(errorHandler(401, 'Unauthorized'))
+        }
+        req.user = user;
+        next();
+    });
+}
