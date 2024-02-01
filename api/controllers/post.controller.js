b301e4a8af479bcea2041421ea2c60309bffd931
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-02-01 18:14:12 +0200

    Create get Posts API Route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from '../utils/error.js'
 import Post from '../models/post.model.js'
+
 export const create = async(req, res, next)=>{
     if(!req.user.isAdmin){
         return next(errorHandler(403, 'You are allowed to create a post'));
@@ -18,4 +19,39 @@ export const create = async(req, res, next)=>{
         next(error)
     }
 
+}
+
+export const getPosts = async(req, res, next)=>{
+
+    try {
+       const startIndex = parseInt(req.query.startIndex) || 0;
+       const limit = parseInt(req.query.limit) || 9;
+       const sortBy = req.query.order === 'asc' ? 1 : -1;
+       const posts = await Post.find({
+        ...(req.query.userId && {userId: req.query.userId}),
+        ...(req.query.category && {category: req.query.category}),
+        ...(req.query.slug && {slug: req.query.slug}),
+        ...(req.query.postId && {_id: req.query.postId}),
+        ...(req.query.searchTerm && {
+            $or: [
+                { title: {$regex: req.query.searchTerm, $options: 'i'}},
+                { content: {$regex: req.query.searchTerm, $options: 'i'}},
+            ],
+        }),
+       }).sort({updatedAt: sortBy}).skip(startIndex).limit(limit);
+
+       const totalPosts = await Post.countDocuments();
+
+       const now = new Date();
+       const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() -1, now.getDate());
+
+       const lastMonthPosts = await Post.countDocuments({createdAt: {$gte: oneMonthAgo},});
+
+       res.status(200).json({
+        posts, totalPosts, lastMonthPosts,
+       });
+
+    } catch (error) {
+        next(error)
+    }
 }
