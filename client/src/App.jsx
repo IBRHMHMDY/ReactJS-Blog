c7156834f2e5f4a44bb8fed85182a4149b169274
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-02-04 18:20:28 +0200

    complete Post Page Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import SignIn from './pages/Login';
 import SignUp from './pages/Signup';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import SinglePost from './pages/SinglePost';
+
 import Header from './components/Header';
 import FooterCom from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import AdminPrivateRoute from './components/AdminPrivateRoute';
-import CreatePost from './pages/CreatePost';
-import EditPost from './pages/EditPost';
 
 export default function App() {
   return (
@@ -18,17 +20,18 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route element={<PrivateRoute/>}>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-        </Route>
-        <Route element={<AdminPrivateRoute/>}>
-          <Route path='/create' element={<CreatePost/>}/>
-          <Route path='/edit/:postId' element={<EditPost/>}/>
-        </Route>
         <Route path='/about' element={<About/>}/>
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/login' element={<SignIn/>}/>
+        <Route element={<AdminPrivateRoute/>}>
+          <Route path='/create' element={<CreatePost/>}/>
+          <Route path='/edit/:postId' element={<EditPost/>}/>
+        </Route>
+        <Route element={<PrivateRoute/>}>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
+        <Route path='/post/:postSlug' element={<SinglePost/>}/>
       </Routes>
       <FooterCom/>
     </BrowserRouter >
---
+++ client/src/pages/SinglePost.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import {Link, useParams} from 'react-router-dom';
+import {Button, Spinner} from 'flowbite-react'
+
+export default function SinglePost() {
+  const {postSlug} = useParams();
+  const [loading, setLoading]=useState(false);
+  const [error, setError]=useState(false);
+  const [post, setPost]=useState(null)
+
+  useEffect(()=>{
+    const fetchPost = async()=>{
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+        const data = await res.json();
+        if(!res.ok){
+          setError(true);
+          setLoading(false)
+          return;
+        }
+        if(res.ok){
+          setLoading(false)
+          setError(null)
+          setPost(data.posts[0]);
+        }
+      } catch (error) {
+        setError(error.message);
+        setLoading(false)
+      }
+    }
+    fetchPost()
+  }, [postSlug])
+
+  if(loading) return <div>
+    <div className='flex justify-center items-center min-h-screen'>
+      <Spinner size='xl'/>
+    </div>
+  </div>
+  return (
+    <main className='flex flex-col max-w-6xl mx-auto min-h-screen p-3'>
+      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl
+      '>{post && post.title}</h1>
+      <Link to={`/search?category=${post && post.category}`} className='self-center mt-5'>
+        <Button color='gray' pill size='xs'>{post && post.category}</Button>
+      </Link>
+      <img src={post && post.image} alt={post && post.title} 
+            className='max-h-[600px] w-full object-cover p-3 mt-10' />
+      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className='italic'>{post & (post?.content?.length /1000).toFixed(0)} mins read</span>
+      </div>
+      <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}></div>
+    </main>
+  )
+}
