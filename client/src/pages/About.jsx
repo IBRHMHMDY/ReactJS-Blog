177b9dbb5b3e5d21d7d7cbca5061a6a034648671
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-02-06 21:06:09 +0200

    Complete About and Projects Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='flex items-center justify-center my-5'>
+      <div className='max-w-3xl text-center mx-auto'>
+        <div className='p-5'>
+          <h1 className='text-center font-bold text-3xl my-5'>About Selfstudy</h1>
+          <div className='flex flex-col gap-6 text-gray-500 text-xl my-5'>
+            <p>
+              Selfstudy blog is an educational platform specializing in programming, with a primary focus on key programming languages such as JavaScript, PHP, and Python. The blog aims to provide rich and detailed content to assist beginners and intermediate programmers in developing their skills in these programming languages.
+            </p>
+            <p>
+              Selfstudy offers articles and tutorials that are simplified and systematic, including explanations and practical examples to facilitate the understanding of programming concepts. The blog covers a variety of topics, including fundamental and advanced programming concepts, such as data structures, graphical user interface design, and database programming.
+            </p>
+            <p>
+              Thanks to its emphasis on popular programming languages, Selfstudy is considered a reliable and valuable resource for learners seeking to acquire strong software development skills. The blog encourages ongoing self-study and provides the necessary support for individuals striving for success in the field of programming.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+
+
+
+
+
+
+// مدونة Selfstudy هي منصة تعليمية متخصصة في مجالات البرمجة، مركزة بشكل أساسي على لغات البرمجة الرئيسية مثل JavaScript، PHP، وPython. تهدف المدونة إلى توفير محتوى غني ومفصل يساعد الطلاب والمبرمجين المبتدئين والمتوسطين على تطوير مهاراتهم في هذه اللغات البرمجية.
+
+// يقدم Selfstudy مقالات ودروس تعليمية مبسطة ومنهجية، تشمل شروحات وأمثلة عملية لتسهيل فهم المفاهيم البرمجية. تتنوع المواضيع في المدونة لتشمل مفاهيم أساسية ومتقدمة في البرمجة، مثل هياكل البيانات، وتصميم الواجهات الرسومية، وبرمجة قواعد البيانات.
+
+// بفضل تركيزها الرئيسي على لغات البرمجة الشهيرة، تُعتبر Selfstudy مصدرًا موثوقًا ومفيدًا للمتعلمين الذين يسعون لاكتساب مهارات قوية في تطوير البرمجيات. تشجع المدونة على الاستمرار في الدراسة الذاتية وتوفير الدعم اللازم للأفراد الذين يسعون لتحقيق نجاح في مجال البرمجة.
+
+
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import CallToActions from '../components/CallToActions'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='flex flex-col gap-4 my-20'>
+      <h1 className='text-3xl font-bold text-center '>Projects</h1>
+      <p className='text-center text-gray-500 text-xl'>Build fun and engaging projects while learning HTML, CSS and Javascript!</p>
+      <div className='flex justify-center p-5'>
+        <CallToActions />
+      </div>
+    </div>
   )
 }
